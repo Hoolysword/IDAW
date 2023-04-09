dca6b9897cc67c39887baba7ab83038cb3afd892
Robin <bukielskirobin@gmail.com>
2023-04-09 20:24:16 +0200

    commit

--- projet/backend/init_db.php
+++ projet/backend/init_db.php
@@ -6,21 +6,15 @@ $connectionString .= ";port=". _MYSQL_PORT;
 $connectionString .= ";dbname=" . _MYSQL_DBNAME;
 $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
 $info = new PDO("mysql:host="._MYSQL_HOST.";port=". _MYSQL_PORT.";dbname=information_schema",_MYSQL_USER,_MYSQL_PASSWORD,$options);
-$drop =$info->prepare("SELECT table_name FROM tables WHERE table_schema =". _MYSQL_DBNAME);
+$drop =$info->prepare("DROP DATABASE ". _MYSQL_DBNAME);
 $drop->execute();
-$tables = $drop->fetchAll();
-try {
+$create =$info->prepare("CREATE DATABASE " . _MYSQL_DBNAME);
+$create->execute();
 $pdo = new PDO($connectionString,_MYSQL_USER,_MYSQL_PASSWORD,$options);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $erreur) {
-//myLog('Erreur : '.$erreur->getMessage());
-}
-foreach($tables as $table){
-    $delete= "DROP TABLE"." ".$table[0];
-    $pdo->prepare($delete)->execute();
-}
-$sql = file_get_contents('sql/idaw.sql');
+
+
+$sql = file_get_contents(__DIR__ . "/sql/idaw.sql");
 $pdo->exec($sql);
 
 ?>
--- projet/frontend/templates/login.php
+++ projet/frontend/templates/login.php
@@ -4,6 +4,7 @@
         session_start();
         session_unset();
         session_destroy();
+       
 ?>
 
 <head>
