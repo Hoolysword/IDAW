fce346973bebfc64f10f3395104539c991a9061a
TheDadi59 <di.danielnd59860@gmail.com>
2023-04-09 20:39:54 +0200

    login modif

--- projet/frontend/templates/login.php
+++ projet/frontend/templates/login.php
@@ -4,7 +4,7 @@
         session_start();
         session_unset();
         session_destroy();
-        require_once("../../backend/init_db.php");
+        // require_once("../../backend/init_db.php");
 ?>
 
 <head>
